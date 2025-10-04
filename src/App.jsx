b3e110c9f5af1fafb49ed3c5378134ce40460740
oleksii-petrenko-metagenics <oleksiipetrenko@metagenics.com>
2025-10-04 16:22:49 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="mx-auto max-w-4xl px-4 sm:px-6 lg:px-8 py-8">
+      <h1 className="text-violet-300">111</h1>
+    </main>
+  );
+}
+
+export default App;
