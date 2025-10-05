f4d8a26222c102ab1a493571bcba9beb08d8e906
oleksii-petrenko-metagenics <oleksiipetrenko@metagenics.com>
2025-10-05 19:11:56 +0300

    added Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Hero from "./components/Hero";
+
 function App() {
   return (
-    <main className="mx-auto max-w-4xl px-4 sm:px-6 lg:px-8 py-8">
-      <h1 className="text-violet-300">111</h1>
+    <main className="relative min-h-sceen w-screen overflow-x-hidden">
+      <Hero />
     </main>
   );
 }
