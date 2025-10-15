eaa455043ef7e32d87ae9061a418cbbb30155a21
oleksii-petrenko-metagenics <oleksiipetrenko@metagenics.com>
2025-10-15 22:55:37 +0300

    fixed size of image

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
-import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 import { ScrollTrigger } from "gsap/all";
 
@@ -19,8 +19,8 @@ function About() {
     });
 
     clipAnimation.to(".mask-clip-path", {
-      width: "10dvh",
-      height: "100dvh",
+      width: "100vw",
+      height: "100vh",
       borderRadius: 0,
     });
 
@@ -37,16 +37,19 @@ function About() {
         </div>
 
         <div className="about-subtext">
-          <p>The Game of Games begins - your life, now an epic MMORG</p>
-          <p>Zentry unites every player from countless games and platforms</p>
+          <p>The Game of Games begins—your life, now an epic MMORPG</p>
+          <p className="text-gray-500">
+            Zentry unites every player from countless games and platforms, both
+            digital and physical, into a unified Play Economy
+          </p>
         </div>
       </div>
 
       <div className="h-dvh w-screen" id="clip">
         <div className="mask-clip-path about-image">
           <img
-            src="/img/about.webp"
-            alt="about"
+            src="img/about.webp"
+            alt="Background"
             className="absolute left-0 top-0 size-full object-cover"
           />
         </div>
