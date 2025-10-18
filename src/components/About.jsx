fc52863fe24004ae955272e1bcad25917959b138
oleksii-petrenko-metagenics <oleksiipetrenko@metagenics.com>
2025-10-18 20:45:15 +0300

    Added Animated Title component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,6 +3,8 @@ import { useGSAP } from "@gsap/react";
 
 import { ScrollTrigger } from "gsap/all";
 
+import AnimatedTitle from "./AnimatedTitle";
+
 gsap.registerPlugin(ScrollTrigger);
 
 function About() {
@@ -23,18 +25,19 @@ function About() {
       height: "100vh",
       borderRadius: 0,
     });
-
-    clipAnimation.to(".story-img-content", {});
   });
+
   return (
     <div id="about" className="min-h-screen w-screen">
       <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
-        <h2 className="font-general text-sm uppercase md:text-[10px]">
+        <p className="font-general text-sm uppercase md:text-[10px]">
           Welcome to Zentry
-        </h2>
-        <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">
-          Disc<b>o</b>ver the world's <br />l<b>a</b>rgest shared adventure
-        </div>
+        </p>
+
+        <AnimatedTitle
+          title="Disc<b>o</b>ver the world's <br /> largest shared <b>a</b>dventure"
+          containerClass="mt-5 !text-black text-center"
+        />
 
         <div className="about-subtext">
           <p>The Game of Games begins—your life, now an epic MMORPG</p>
